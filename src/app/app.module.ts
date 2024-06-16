fccc1e181d438851058b6c1225fde9bfb168cfde
sjabar <you@example.comjabarsadiqsi@gmail.com>
2024-06-16 11:45:23 +0530

    stock exchange

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppRoutingModule } from './app.routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [ 
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators';
+import { UserToken } from '../models/user-token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private tokenSubject: BehaviorSubject<UserToken>;
+  public token: Observable<UserToken>;
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {
+    this.tokenSubject = new BehaviorSubject<UserToken>(JSON.parse(localStorage.getItem('user-token')||''));
+    this.token = this.tokenSubject.asObservable();
+   }
+
+  public get tokenValue(): UserToken {
+    return this.tokenSubject.value;
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<UserToken>(`http://localhost:3000/users/login`, {email, password})
+    .pipe(
+      map(token => {
+        const userToken: UserToken = token;
+
+        localStorage.setItem('user-token', JSON.stringify(userToken));
+        this.tokenSubject.next(userToken);
+
+        return userToken;
+      })
+    );
+  }
+
+  logout() {
+    //let userToken: UserToken=null;
+    localStorage.removeItem('user-token');
+    let userToken: UserToken=new UserToken();
+    this.tokenSubject.next(userToken);
+    this.router.navigate(['/']);
+  }
+}
